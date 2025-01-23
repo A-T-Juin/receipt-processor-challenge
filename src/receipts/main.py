eb13318fb5f78daffd35b09e8265a53320a692de
Allan Tham <a.t.juin@gmail.com>
2025-01-22 18:31:34 -0800

    Added point look up endpoint along with tests for success & failure

--- src/receipts/main.py
+++ src/receipts/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+volatile_memory = {
+    "1000": 500, # test data
+}
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# grab points from our db
+@app.get("/receipts/{receipt_id}/points")
+async def get_points_by_receipt_id(receipt_id: str):
+    # receipt_id found
+    if receipt_id in volatile_memory:
+        return {"points": volatile_memory[receipt_id]}
+    # receipt_id not found
+    else:
+        return JSONResponse(
+            status_code=404,
+            content={"description": "No receipt found for that ID."}
+        )
--- tests/receipts/test_receipts_service.py
+++ tests/receipts/test_receipts_service.py
@@ -10,3 +10,17 @@ def test_root():
   assert response.json() == {
     "message": "Hello World"
   }
+
+def test_get_points_by_receipt_id_success():
+  response = client.get("/receipts/1000/points")
+  assert response.status_code == 200
+  assert response.json() == {
+    "points": 500
+  }
+
+def test_get_points_by_receipt_id_failure():
+  response = client.get("/receipts/99/points")
+  assert response.status_code == 404
+  assert response.json() == {
+    "description": "No receipt found for that ID."
+  }
